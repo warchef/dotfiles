5e9ab7585d0cc60d529886b6be4abf3aa1d88883
warchef <koncz.istvan.marton@gmail.com>
2025-08-22 19:15:30 +0200

    Update install.sh

--- arch/install.sh
+++ arch/install.sh
@@ -1,5 +1,5 @@
-sudo pacman -Syu samba pcmanfm gvfs gvfs-smb xorg-server xorg-xinit xorg-xrandr xorg-xsetroot git lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings base-devel libx11 libxft vlc postgis libreoffice-fresh-hu steam lutris calibre qbittorrent glab github-cli vim nano discord telegram-desktop code zip unzip xarchiver qemu-full virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat openvpn freerdp2 btop htop xterm cmake ninja redis pavucontrol mc brightnessctl ranger
+sudo pacman -Syu -needed samba pcmanfm gvfs gvfs-smb xorg-server xorg-xinit xorg-xrandr xorg-xsetroot base-devel libx11 libxft mpv postgis libreoffice-fresh-hu steam lutris qbittorrent glab github-cli gvim nano discord telegram-desktop code zip unzip xarchiver qemu-full virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat openvpn freerdp2 btop htop xterm cmake ninja redis pavucontrol mc brightnessctl ranger
 
 sudo usermod -aG libvirt marci
-sudo systemctl enable --now libvirtd
-sudo systemctl enable lightdm
+#sudo systemctl enable --now libvirtd
+#sudo systemctl enable lightdm
