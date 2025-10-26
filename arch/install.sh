0981cbfab4f4c6e16cf0c8124478a3a0915b7ba8
warchef <koncz.istvan.marton@gmail.com>
2025-10-26 19:26:29 +0100

    Update package installation in install.sh

--- arch/install.sh
+++ arch/install.sh
@@ -1,4 +1,4 @@
-sudo pacman -Syu --needed impala bluetui samba pcmanfm gvfs gvfs-smb xorg-server xorg-xinit xorg-xrandr xorg-xsetroot base-devel libx11 libxft mpv postgis wine-staging  docker docker-buildx docker-compose libreoffice-fresh-hu steam lutris transmission-gtk dbeaver glab github-cli gvim nano neovim luajit fd fzf	ripgrep tree-sitter-cli	ttf-jetbrains-mono-nerd ttf-jetbrains-mono discord telegram-desktop code zip unzip p7zip xarchiver qemu-full virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat openvpn freerdp btop kitty mise uv remmina cmake ninja gdb redis pavucontrol mc brightnessctl ranger giflib lib32-giflib gnutls lib32-gnutls v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib sqlite lib32-sqlite libxcomposite lib32-libxcomposite ocl-icd lib32-ocl-icd libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader sdl2-compat lib32-sdl2-compat 
+sudo pacman -Syu --needed impala bluetui samba pcmanfm gvfs gvfs-smb xorg-server xorg-xinit xorg-xrandr xorg-xsetroot base-devel libffi libyaml openssl zlib libx11 libxft mpv postgis wine-staging docker docker-buildx docker-compose libreoffice-fresh-hu steam lutris transmission-gtk dbeaver glab github-cli gvim nano neovim luajit fd fzf	ripgrep tree-sitter-cli	ttf-jetbrains-mono-nerd ttf-jetbrains-mono discord telegram-desktop feh lazygit lazydocker code zip unzip p7zip xarchiver qemu-full virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat openvpn freerdp btop kitty mise uv remmina cmake ninja clang gdb redis pavucontrol mc brightnessctl ranger giflib lib32-giflib gnutls lib32-gnutls v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib sqlite lib32-sqlite libxcomposite lib32-libxcomposite ocl-icd lib32-ocl-icd libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader sdl2-compat lib32-sdl2-compat 
 
 sudo usermod -aG libvirt marci
 sudo systemctl enable --now libvirtd
@@ -10,14 +10,11 @@ git clone https://aur.archlinux.org/paru-bin && cd paru-bin && makepkg -si && cd
 paru -Syu google-chrome jetbrains-toolbox spotify brave-bin local-by-flywheel-bin
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo pacman -S --needed base-devel libffi libyaml openssl zlib
 
 mise use -g ruby go erlang elixir
 mise use dotnet
 
 sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/
-
-sudo pacman -S feh lazygit lazydocker
 
 cd ../dwm && sudo make clean install
 cd ../slstatus && sudo make clean install
