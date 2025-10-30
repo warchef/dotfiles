c98eaaa63a45021c912026073a82da1126f2972e
warchef <koncz.istvan.marton@gmail.com>
2025-10-30 20:32:00 +0100

    Update install.sh

--- openbsd/install.sh
+++ openbsd/install.sh
@@ -6,6 +6,7 @@ doas pkg_add ranger mc xclip jetbrains-mono noto-nerd-fonts hack-fonts adobe-sou
 doas cp hostname.iwx0 /etc/
 doas cp mk.conf /etc/
 doas cp sysctl.conf /etc/
+doas cp wxconsctl.conf /etc/
 cp .xsession ~
 cp .bashrc ~
 cp ../.Xresources ~
