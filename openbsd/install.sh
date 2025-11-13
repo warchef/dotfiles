4a6166717daa169fb39bc1f81439eb1674b3b34d
warchef <koncz.istvan.marton@gmail.com>
2025-11-13 17:44:26 +0100

    Modify install script for OpenBSD setup
    Updated package installation command and changed download URLs for OpenBSD installation.

--- openbsd/install.sh
+++ openbsd/install.sh
@@ -2,7 +2,7 @@ doas rcctl enable apmd
 doas rcctl set apmd flags -A
 doas rcctl start apmd
 doas cp installurl /etc/
-doas pkg_add simple-mtpfs ranger mc xclip jetbrains-mono noto-nerd-fonts hack-fonts adobe-source-code-pro noto-emoji noto-fonts noto-cjk feh bash bash-completion vim vim-spell-hu openssl p7zip chromium tdesktop postgresql-server postgresql-contrib postgresql-client postgis redis transmission-gtk libreoffice libreoffice-i18n-hu mupdf mpv intel-media-driver libva-utils htop go fzf uv ruby llvm lldb clang-tools-extra rust jdk elixir rust-analyzer rust-gdb rust-rustfmt rust-clippy node yarn py3-pip ripgrep gmake py3-pipx cmake xmlto libusb1 pcsc-lite gitlab-cli github-cli portslist samba ninja openvpn consolekit2 docker-cli docker-compose dosbox innoextract devilutionx openmw gemrb
+doas pkg_add simple-mtpfs ranger mc xclip jetbrains-mono noto-nerd-fonts noto-emoji noto-fonts noto-cjk feh bash bash-completion vim vim-spell-hu openssl p7zip chromium tdesktop postgresql-server postgresql-contrib postgresql-client postgis redis transmission-gtk libreoffice libreoffice-i18n-hu mupdf mpv intel-media-driver libva-utils htop go fzf uv ruby jdk elixir node yarn py3-pip gmake py3-pipx cmake xmlto libusb1 pcsc-lite gitlab-cli github-cli portslist samba ninja openvpn consolekit2 docker-cli docker-compose dosbox innoextract devilutionx openmw gemrb
 doas cp hostname.iwx0 /etc/
 doas cp mk.conf /etc/
 doas cp sysctl.conf /etc/
@@ -21,13 +21,15 @@ doas rcctl enable messagebus
 doas rcctl enable xenodm
 doas rcctl enable vmd
 doas chsh -s /usr/local/bin/bash marci
-cd /tmp && ftp https://ftp2.eu.openbsd.org/pub/OpenBSD/$(uname -r)/{ports.tar.gz,SHA256.sig}
-cd /usr && doas tar xzf /tmp/ports.tar.gz
+cd /usr
+doas cvs -z9 -qd anoncvs@mirror.osn.de:/cvs checkout -P src
+doas cvs -z9 -qd anoncvs@mirror.osn.de:/cvs checkout -P ports
+doas cvs -z9 -qd anoncvs@mirror.osn.de:/cvs checkout -P xenocara
 cd ~
 mkdir vm
 cd vm
 mkdir iso
 cd iso
-ftp https://ftp2.eu.openbsd.org/pub/OpenBSD/7.8/amd64/install78.iso
+ftp https://ftp2.eu.openbsd.org/pub/OpenBSD/snapshots/amd64/install78.iso
 ftp https://cdimage.debian.org/cdimage/archive/12.12.0/amd64/iso-cd/debian-12.12.0-amd64-netinst.iso
 ftp https://dl-cdn.alpinelinux.org/alpine/v3.22/releases/x86_64/alpine-standard-3.22.2-x86_64.iso
