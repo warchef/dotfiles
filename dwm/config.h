9369b2c4f1c36391941e55e30fde11572cb88e1a
warchef <koncz.istvan.marton@gmail.com>
2025-11-15 15:43:24 +0100

    Add nautilus

--- dwm/config.h
+++ dwm/config.h
@@ -62,6 +62,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *chromecmd[]  = { "google-chrome-stable", NULL };
+static const char *nautiluscmd[] = { "nautilus", NULL };
 
 static const char *upvol[]      = { "/usr/bin/wpctl",   "set-volume", "@DEFAULT_AUDIO_SINK@",      "5%+",      NULL };
 static const char *downvol[]    = { "/usr/bin/wpctl",   "set-volume", "@DEFAULT_AUDIO_SINK@",      "5%-",      NULL };
@@ -111,6 +112,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY|ShiftMask,             XK_b,      spawn,           {.v = chromecmd } },
+	{ MODKEY|ShiftMask,		XK_n,	   spawn,	   {.v = nautiluscmd } },	
 };
 
 /* button definitions */
@@ -129,3 +131,4 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
