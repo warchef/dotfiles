486cd24fe975dd73480da4996cfc24a04e3c2992
warchef <koncz.istvan.marton@gmail.com>
2025-12-30 11:06:11 +0100

    Refactor install_snapshot.sh to streamline setup

--- openbsd/install_snapshot.sh
+++ openbsd/install_snapshot.sh
@@ -1,26 +1,3 @@
-doas rcctl enable apmd
-doas rcctl set apmd flags -A
-doas rcctl start apmd
-doas cp hostname.iwx0 /etc/
-doas sh /etc/netstart
-doas cp installurl /etc/
-doas pkg_add eza simple-mtpfs ranger mc xclip jetbrains-mono noto-nerd-fonts noto-emoji noto-fonts noto-cjk vim vim-spell-hu feh bash bash-completion openssl p7zip ungoogled-chromium tor-browser postgresql-server postgresql-contrib postgresql-client redis transmission-gtk libreoffice libreoffice-i18n-hu mupdf mpv rust intel-media-driver libva-utils htop go fzf uv ruby jdk elixir node yarn llvm rust rust-gdb rust-analyzer rust-clippy rust-rustfmt clang-tools-extra py3-pip gmake py3-pipx cmake xmlto libusb1 pcsc-lite gitlab-cli github-cli portslist ninja openvpn consolekit2 docker-cli docker-compose dosbox-x innoextract devilutionx openmw gemrb
-doas cp mk.conf /etc/
-doas cp sysctl.conf /etc/
-doas cp wsconsctl.conf /etc/
-cp .Xresources ~
-mkdir -p ~/.config/fontconfig/conf.d
-cp fonts.conf ~/.config/fontconfig/conf.d/
-fc-cache -fv
-cp .xsession ~
-cp .bashrc ~
-mkdir ~/wallpaper && cp ../wall1.jpg ~/wallpaper/
-cd suckless/dwm && doas make clean install && make clean
-cd ../dmenu && doas make clean install && make clean
-cd ../slstatus && doas make clean install && make clean
-doas rcctl enable messagebus
-doas rcctl enable xenodm
-doas rcctl enable vmd
-doas chsh -s /usr/local/bin/bash marci
+sh base.sh
 cd /usr
 doas cvs -z9 -qd anoncvs@mirror.osn.de:/cvs checkout -P ports
