b559c5dff165c259eb6eff81ce3297c874539a4a
warchef <koncz.istvan.marton@gmail.com>
2025-12-30 10:47:09 +0100

    Clean up install script by removing unused commands
    Removed unnecessary checkout commands and ISO downloads.

--- openbsd/install.sh
+++ openbsd/install.sh
@@ -23,13 +23,4 @@ doas rcctl enable xenodm
 doas rcctl enable vmd
 doas chsh -s /usr/local/bin/bash marci
 cd /usr
-doas cvs -z9 -qd anoncvs@mirror.osn.de:/cvs checkout -P src
 doas cvs -z9 -qd anoncvs@mirror.osn.de:/cvs checkout -P ports
-doas cvs -z9 -qd anoncvs@mirror.osn.de:/cvs checkout -P xenocara
-cd ~
-mkdir vm
-cd vm
-mkdir iso
-cd iso
-ftp https://ftp2.eu.openbsd.org/pub/OpenBSD/snapshots/amd64/install78.iso
-ftp https://cdimage.debian.org/cdimage/archive/12.12.0/amd64/iso-cd/debian-12.12.0-amd64-netinst.iso
