19a46037d2a7242df4a73c1f8420712f3c1bbff7
warchef <koncz.istvan.marton@gmail.com>
2026-01-01 18:35:28 +0100

    Change browser command from Google Chrome to Chromium

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -61,7 +61,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *chromecmd[]  = { "google-chrome-stable", NULL };
+static const char *chromecmd[]  = { "chromium", NULL };
 static const char *nautiluscmd[] = { "nautilus", NULL };
 
 static const char *upvol[]      = { "/usr/bin/wpctl",   "set-volume", "@DEFAULT_AUDIO_SINK@",      "5%+",      NULL };
