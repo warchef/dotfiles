6350286b48de3b2b50f4e137b1aac6777f747b31
warchef <koncz.istvan.marton@gmail.com>
2026-01-01 19:30:57 +0100

    Fix stable ports

--- openbsd/install_stable.sh
+++ openbsd/install_stable.sh
@@ -26,4 +26,4 @@ cd /tmp
 ftp https://ftp2.eu.openbsd.org/pub/OpenBSD/$(uname -r)/{ports.tar.gz,SHA256.sig}
 signify -Cp /etc/signify/openbsd-$(uname -r | cut -c 1,3)-base.pub -x SHA256.sig ports.tar.gz
 cd /usr
-tar xzf /tmp/ports.tar.gz
+doas tar xzf /tmp/ports.tar.gz
