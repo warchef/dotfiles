6e423f62b46e771d97eeff420b10dc11da1d9a14
warchef <koncz.istvan.marton@gmail.com>
2026-01-08 11:43:43 +0100

    Remove duplicate FTP command for Debian ISO

--- openbsd/vm_src.sh
+++ openbsd/vm_src.sh
@@ -7,4 +7,3 @@ cd vm
 mkdir iso
 cd iso
 ftp https://ftp2.eu.openbsd.org/pub/OpenBSD/snapshots/amd64/install78.iso
-ftp https://cdimage.debian.org/cdimage/archive/12.12.0/amd64/iso-cd/debian-12.12.0-amd64-netinst.iso
