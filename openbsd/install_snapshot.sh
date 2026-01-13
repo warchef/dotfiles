12571a19bc7b0109597d07957ff48834f5695731
warchef <koncz.istvan.marton@gmail.com>
2026-01-13 14:25:35 +0100

    Refactor install_snapshot.sh for efficiency
    Removed unnecessary commands and added CVS checkout for ports.

--- openbsd/install_snapshot.sh
+++ openbsd/install_snapshot.sh
@@ -1,27 +1,2 @@
-doas rcctl enable apmd
-doas rcctl set apmd flags -A
-doas rcctl start apmd
-doas cp hostname.iwx0 /etc/
-doas sh /etc/netstart
-doas cp installurl /etc/
-doas pkg_add python eza ranger mc xclip jetbrains-mono profont-nerd-fonts noto-emoji noto-fonts noto-cjk vim vim-spell-hu feh bash bash-completion openssl p7zip tdesktop chromium tor-browser transmission-gtk libreoffice libreoffice-i18n-hu mupdf mpv intel-media-driver libva-utils htop fzf gmake cmake xmlto libusb1 pcsc-lite portslist ninja openvpn consolekit2 dosbox-x innoextract devilutionx openmw gemrb
-doas pkg_add postgresql-server postgresql-contrib postgresql-client redis rust go uv ruby jdk elixir node yarn rust-gdb rust-analyzer rust-clippy rust-rustfmt clang-tools-extra py3-pip py3-pipx gitlab-cli github-cli docker-cli docker-compose 
-doas cp mk.conf /etc/
-doas cp sysctl.conf /etc/
-doas cp wsconsctl.conf /etc/
-cp .Xresources ~
-mkdir -p ~/.config/fontconfig/conf.d
-cp fonts.conf ~/.config/fontconfig/conf.d/
-fc-cache -fv
-cp .xsession ~
-cp .bashrc ~
-mkdir ~/wallpaper && cp ../wall1.jpg ~/wallpaper/
-cd suckless/dwm && doas make clean install && make clean
-cd ../dmenu && doas make clean install && make clean
-cd ../slstatus && doas make clean install && make clean
-doas rcctl enable messagebus
-doas rcctl enable xenodm
-doas rcctl enable vmd
-doas chsh -s /usr/local/bin/bash marci
 cd /usr
 doas cvs -z9 -qd anoncvs@mirror.osn.de:/cvs checkout -P ports
