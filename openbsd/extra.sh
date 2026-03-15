b313aa33e449695e8849a837178427acb7e97f44
warchef <koncz.istvan.marton@gmail.com>
2026-03-15 16:56:06 +0100

    Update OpenBSD ISO download to version 79

--- openbsd/extra.sh
+++ openbsd/extra.sh
@@ -3,7 +3,7 @@ mkdir vm
 cd vm
 mkdir iso
 cd iso
-ftp https://ftp2.eu.openbsd.org/pub/OpenBSD/snapshots/amd64/install78.iso
+ftp https://ftp2.eu.openbsd.org/pub/OpenBSD/snapshots/amd64/install79.iso
 
 doas pkg_add tor-browser transmission-gtk libreoffice libreoffice-i18n-hu mupdf mpv htop fzf gmake dosbox-x innoextract devilutionx openmw gemrb openwv
 doas pkg_add postgresql-server postgresql-contrib postgresql-client redis rust go uv ruby jdk elixir node yarn rust-gdb rust-analyzer rust-clippy rust-rustfmt clang-tools-extra py3-pip py3-pipx gitlab-cli github-cli docker-cli docker-compose 
